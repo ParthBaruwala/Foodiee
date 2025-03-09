5f56d45105d4343cb4dea5953752865f39a3e982
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2025-03-10 00:01:02 +0530

    UPADATE: itemes recieved

--- app/src/main/java/com/example/foodiee/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/foodiee/Fragment/HistoryFragment.kt
@@ -3,8 +3,10 @@ package com.example.foodiee.Fragment
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Base64
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -50,7 +52,17 @@ class HistoryFragment : Fragment() {
             seeItemsRecentBuy()
         }
 
+        binding.receivedButton.setOnClickListener {
+            updateOrderStatus()
+        }
+
         return binding.root
+    }
+
+    private fun updateOrderStatus() {
+        val itemPushKey = listOfOrderItem[0].itemPushKey
+        val completeOrderReference = database.reference.child("CompletedOrder").child(itemPushKey!!)
+        completeOrderReference.child("paymentReceived").setValue(true)
     }
 
     private fun seeItemsRecentBuy() {
@@ -123,9 +135,11 @@ class HistoryFragment : Fragment() {
                         e.printStackTrace()
                     }
                 }
-
-                listOfOrderItem.reverse()
-                if(listOfOrderItem.isNotEmpty()){
+                val isOrderIsAccepted = listOfOrderItem[0].orderAccepted
+                Log.d("ISORDERISACCEPTED", "setDataInRecentBuyItem: $isOrderIsAccepted")
+                if(isOrderIsAccepted){
+                    orderStatues.background.setTint(Color.GREEN)
+                    receivedButton.visibility = View.VISIBLE
                 }
             }
         }
